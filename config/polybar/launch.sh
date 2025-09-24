6949e0b3afd406bc409cc6a9a432a1b998d67e1e
mudrc <mudbyte1@outlook.com>
2025-09-24 20:23:12 +0200

    Add files via upload
    cooked the rice

---
+++ config/polybar/launch.sh
@@ -0,0 +1,10 @@
+polybar-msg cmd quit
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
